6e20d63692bb1de21d703b4306b949b3564de17d
Markus Kwaśnicki <486983+mrkskwsnck@users.noreply.github.com>
2021-05-15 23:14:35 +0200

    Kernel param supplemented

--- enable_audio_codec_power_management.sh
+++ enable_audio_codec_power_management.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Quelle(n):  c't 25/2016, S. 77
 #             c't 26/2016, S. 12
+#             https://wiki.ubuntuusers.de/Soundkarten_konfigurieren/HDA/#Lautsprecher-knacksen
 # Enable Audio codec power management
 echo 1 > /sys/module/snd_hda_intel/parameters/power_save
+echo Y > /sys/module/snd_hda_intel/parameters/power_save_controller
